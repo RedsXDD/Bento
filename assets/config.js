5f5fb8f57473a0856784e8a3473e1f1fe8f98333
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-22 11:42:43 -0300

    refactor: Updated to own config.

---
+++ assets/config.js
@@ -0,0 +1,63 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// General
+	name: "Reds",
+
+	// Greetings
+	greetingMorning: "おはよう~ <3",
+	greetingAfternoon: "こんにちは~ <3",
+	greetingEvening: "こんばんは~ <3",
+	greetingNight: "寝てください!!! D:",
+
+	// Layout
+	bentoLayout: "bento",
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: "1",
+			name: "Youtube",
+			icon: "youtube",
+			link: "https://youtube.com/",
+		},
+		{
+			id: "2",
+			name: "Facebook",
+			icon: "facebook",
+			link: "https://facebook.com/",
+		},
+		{
+			id: "3",
+			name: "Reddit",
+			icon: "glasses",
+			link: "https://reddit.com",
+		},
+		{
+			id: "4",
+			name: "Github",
+			icon: "github",
+			link: "https://github.com",
+		},
+		{
+			id: "5",
+			name: "X",
+			icon: "twitter",
+			link: "https://x.com",
+		},
+		{
+			id: "6",
+			name: "Gmail",
+			icon: "mail",
+			link: "https://mail.google.com/",
+		},
+	],
+};
